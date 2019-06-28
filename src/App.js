a32e189f5f6f52eaa765dcaac59918dd2ba0c5d3
DanishDude <christian.hinge@gmail.com>
2019-06-28 05:25:45 +0200

    notifs

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './Home'; // React router dom
 import Login from './Login'; // React router dom
 import Result from './Result'; // React router dom
 import Game from './Game'; // React router dom
+import Tips from './Tips'; // React router dom
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
         <Route path='/game' component={Game} />
         <Route path='/login' component={Login} />
         <Route path='/result' component={Result} />
+        <Route path='/teps' component={Tips} />
       </Switch>
     </div>
   );
