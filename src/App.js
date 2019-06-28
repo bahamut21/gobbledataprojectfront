1c4dae09d274848660a8e271fdb0a702f5709617
Alex Dogoum <lexhoo@yahoo.fr>
2019-06-28 03:34:25 +0200

    form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
 import Formule from './Formulaireinscription/Formule'; // React router dom
-
+import Title from './Formulaireinscription/Title'
 
 function App() {
   return (
     <div className="App">
+      <Title />
       <Formule />   
     </div>
   );
---
+++ src/Formulaireinscription/Boutons.jsx
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import './Boutons.css';
+
+class Boutons extends Component {
+    render() {
+        return(
+            <div>
+            <button className="Gobbled">Envoyer</button>
+            </div>
+        )
+    }
+
+
+    
+}
+
+export default Boutons; 
