035ec7fbd71ff18f1516fc0c884b990df450f311
DanishDude <christian.hinge@gmail.com>
2019-06-28 09:22:44 +0200

    wip

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './Home'; // React router dom
 import Login from './Login'; // React router dom
 import Result from './Result'; // React router dom
 import Game from './Game'; // React router dom
+import Tips from './Tips';
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
         <Route path='/game' component={Game} />
         <Route path='/login' component={Login} />
         <Route path='/result' component={Result} />
+        <Route path='/tips' component={Tips} />
       </Switch>
     </div>
   );
