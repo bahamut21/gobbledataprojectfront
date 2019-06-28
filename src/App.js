39340fd4f9efee17c3d8ab7487a58a16802fbd28
Alex Dogoum <lexhoo@yahoo.fr>
2019-06-28 15:06:13 +0200

    uplayer

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import './App.css';
 import Formule from './Formulaireinscription/Formule'; // React router dom
 import Title from './Formulaireinscription/Title'
+import Player from './Formulaireinscription/players';
 
 
 function App() {
   return (
     <div className="App">      
       <Title />
-      <Formule />   
+      <Formule />  
+      <Player />
     </div>
   );
 }
